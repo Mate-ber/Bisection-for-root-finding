175576c40bf049cd1e9904c6243de5e28637678b
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-01-08 23:04:18 +0700

    Add files via upload

---
+++ funop2.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define double long double
+using namespace std;
+const double eps=1e-9;
+const double alp=1e-4;
+double fun1(double x,double number){
+    return 0.5*(x+number/x);
+}
+double fun2(double x){
+	return x-alp*(x*x-x-4);
+}
+void sqrtfun(){
+	double num=100.0;
+    double g=2.0;
+    int it=0;
+	while(1){
+		it++;
+		double g_next=fun1(g,num);
+		if(fabs(g_next-g)<eps){
+			g=g_next;
+			break;
+		}
+		g=g_next;
+		if(it==100000){
+			cout<<"invalid num"<<endl;
+			return;
+		}
+	}
+    cout<<"Num: "<<num<<" guess: "<<g<<" itterations: "<<it<<endl;
+}
+void modifiedalpha(){
+	double g=2.0;
+	int it=0;
+	while(1){
+		it++;
+		double g_next=fun2(g);
+		if(fabs(g_next-g)<eps){
+			g=g_next;
+			break;
+		}
+		g=g_next;
+		if(it==100000){
+			cout<<"invalid"<<endl;
+			return;
+		}
+	}
+	cout<<"guess(for x^x-4): "<<g<<" itterations: "<<it<<endl;
+}
+int main(){
+	cout<<setprecision(9)<<fixed;
+	sqrtfun();
+    modifiedalpha();
+    return 0;
+}

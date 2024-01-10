0ec08e1b55a3c101d8bbaa85204036ee7ebc8d43
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-01-10 11:43:43 +0700

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -16,11 +16,14 @@ if __name__ == "__main__":
     file_path1 = 'data1.txt'
     file_path2 = 'data2.txt'
     file_path3 = 'data3.txt'
+    file_path4 = 'data4.txt'
 
     x1, y1 = read_data(file_path1)
     x2, y2 = read_data(file_path2)
     x3, y3 = read_data(file_path3)
+    x4, y4 = read_data(file_path4)
 
+    plot_graph(y4, x4, label='fixed point method')
     plot_graph(y3, x3, label='bisection')
     plot_graph(y1, x1, label='Newton')
     plot_graph(y2, x2, label='Newton and bisection')

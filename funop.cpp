851e8f2adbb54c55c923b6595bbd911a10eb3fff
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-01-10 11:32:52 +0700

    Update funop.cpp

--- funop.cpp
+++ funop.cpp
@@ -3,20 +3,23 @@
 using namespace std;
 const double eps=1e-9;
 double fun(double x){
-	return 15*x-5000-x*(10-5/(1-exp(-x)));
+//	return 15*x-5000-x*(10-5/(1-exp(-x)));
 //  return x*x-4;
+	return 2*sin(x)-2;
 }
 int main(){
 	cout<<setprecision(10)<<fixed;
-	double l=eps,r=1e9;
+	
+//	ofstream cout;
+//    cout.open ("data3.txt");
+    
+	double l=-2,r=2;
 	int it=0;
 	while(r-l>eps){
 		it++;
 		double m=(l+r)/2;
-		if(fabs(fun(m))<=eps){
-			cout<<"result:"<<m<<" iterations:"<<it<<endl;
-			return 0;
-		}
+		
+		cout<<r-l<<" "<<it<<endl;
 		if(fun(l)*fun(m)<0){
 			r=m;
 		}
@@ -24,6 +27,6 @@ int main(){
 			l=m;
 		}
 	}
-	cout<<"result:"<<(l+r)/2<<" iterations:"<<it<<endl;
+//	cout<<"result:"<<(l+r)/2<<" iterations:"<<it<<endl;
 	return 0;
 }

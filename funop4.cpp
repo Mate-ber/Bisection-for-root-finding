35f24a7dcb88722c0f4db747d0c414b5fdddcef7
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2024-01-10 11:19:43 +0700

    Update funop4.cpp

--- funop4.cpp
+++ funop4.cpp
@@ -3,53 +3,55 @@
 using namespace std;
 const double eps=1e-9;
 double fun1(double x){
-	return (x-3)*(x-1)*(x-1);
+	return 2*sin(x)-2;
 }
 double fun2(double x){
-	return (3*x-7)*(x-1);
+	return 2*cos(x);
 }
 double newton(double g){
 	return g-fun1(g)/fun2(g);
 }
 int main(){
 	cout<<setprecision(9)<<fixed;
-	double g=-2.0;
+//    ofstream cout;
+//    cout.open ("data2.txt");
+    
+	double g=0;
 	int it=0;
-	double l=-10000,r=10000;
+	double l=-2,r=2;
 	
 	while(r-l>eps){
 		it++;
 		double m=(l+r)/2;
 		double new_g=newton(g);
-		if(fabs(fun1(m))<=eps){
-			cout<<"(bisection)g: "<<m<<" it: "<<it<<endl;
-			return 0;
-		}
 		
-		else if(fabs(new_g-g)<=eps){
-			cout<<"(newton)g: "<<new_g<<" it: "<<it<<endl;
-			return 0;
+		cout<<min(fabs(new_g-g),(r-l))<<" "<<it<<endl;
+		cout<<fabs(new_g-g)<<" "<<it<<endl;
+		
+		if(fabs(new_g-g)<=eps){
+//			cout<<"(newton)g: "<<new_g<<" it: "<<it<<endl;
+			return 0;	
 		}
 		
 		else if(fun1(l)*fun1(m)<0){
 			r=m;
-			if(l>new_g || m<new_g){
-				g=(m+l)/2;
+			if(l<=new_g && new_g<=r){
+				g=new_g;
 			}
 			else{
-				g=new_g;
+				g=(l+r)/2;
 			}
 		}
 		
 		else{
 			l=m;
-			if(new_g<m || new_g>r){
-				g=(m+r)/2;
+			if(l<=new_g && new_g<=r){
+				g=new_g;
 			}
 			else{
-				g=new_g;
-			}		
+				g=(l+r)/2;
+			}	
 		}
 	}
-	cout<<"(bisection)g: "<<(l+r)/2<<" it:"<<it<<endl;
+//	cout<<"(bisection2)g: "<<(l+r)/2<<" it:"<<it<<endl;
 }
